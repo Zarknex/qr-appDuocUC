5a75a3ed0351d71c42bedf7f75e065749cb6a193
Matias Donoso <ma.donosoo@alumnos.duoc.cl>
2021-10-29 01:35:43 -0300

    R.C 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'password-reset',
@@ -24,7 +26,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/notfound/notfound.module').then( m => m.NotfoundPageModule)
   },
   {
-    
+
     path: '**', redirectTo: 'notfound'
   }
 ];
