a4eae9f02b5013f35623272fc1520096339c5eee
Matias Donoso <ma.donosoo@alumnos.duoc.cl>
2021-10-29 02:37:55 -0300

    Detalles menores

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { LocaldbService } from '../services/localdb.service';
 
